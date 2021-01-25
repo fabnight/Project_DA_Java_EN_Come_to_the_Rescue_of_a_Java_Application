c5b5a2560d378c66f76da6eb10b5ce179822dc43
fabnight <58448176+fabnight@users.noreply.github.com>
2021-01-25 16:11:15 +0100

    Amended to only 2 distinct methods reading/writing
    Update of README

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,47 +1,110 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-/**
- * Simple brute force implementation
- *
- */
 public class ReadSymptomDataFromFile implements ISymptomReader {
-
-	private String filepath;
-	
 	/**
+	 * 1st step reading symptoms from symptoms.txt file
 	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filePathSymptoms
+	 * 
+	 * @return resultSymptom
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
+
+	public void getSymptoms(String filePathSymptoms) throws Exception {
+
+		readFileTxt(filePathSymptoms);
 	}
-	
-	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
+
+	TreeMap<String, Integer> readFileTxt(String filePathSymptoms) throws IOException {
+		TreeMap<String, Integer> resultSymptom = new TreeMap<String, Integer>();
+
+		try {
+			BufferedReader readerSymptom = new BufferedReader(new FileReader(filePathSymptoms));
+			// Counting occurrences for each symptom of the file "symptoms.txt",
+			// alphabetical sorting
+			String line = null;
 			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
+				line = readerSymptom.readLine();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+
+			while (line != null && !line.isEmpty()) {
+				// if (line.isEmpty()) {
+				// line = readerSymptom.readLine();
+				// }
+				if (resultSymptom.containsKey(line)) {
+					resultSymptom.put(line, (resultSymptom.get(line)) + 1);
+				} else {
+					resultSymptom.put(line, 1);
+				}
+				line = readerSymptom.readLine();
+			}
+
+			readerSymptom.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichier symptoms.txt introuvable");
+			System.exit(-1);
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		return result;
+		return resultSymptom;
 	}
 
+	/**
+	 * 2nd Step Put the result to a file "results.out.txt". <b>Generate a list</b>
+	 * counting occurrences of each symptom string in it, <b>one per line</b>. If no
+	 * data is available, return an empty List
+	 * 
+	 * @param filepathresults
+	 * 
+	 * 
+	 * @throws Exception FileNotFoundException if impossible to find folder to
+	 *                   export results.out.txt
+	 */
+	public void writeFileToTxt(String filePathSymptoms, String filePathResults) throws Exception {
+		TreeMap<String, Integer> resultSymptom = readFileTxt(filePathSymptoms);
+		writeFile(filePathResults, resultSymptom);
+	}
+
+	private void writeFile(String filePathResults, TreeMap<String, Integer> resultSymptom) throws Exception {
+
+		try {
+
+			FileWriter file = new FileWriter(filePathResults);
+			BufferedWriter out = new BufferedWriter(file);
+
+			for (Map.Entry<String, Integer> entry : resultSymptom.entrySet()) {
+				out.write(entry.getKey() + "=" + entry.getValue());
+				out.newLine();
+			}
+			out.flush();
+			out.close();
+
+		} catch (
+
+		FileNotFoundException e) {
+			System.out.println("répertoire pour créer results.out.txt introuvable");
+			System.exit(-1);
+
+		}
+
+		System.out.println("terminé");
+	}
+
+	@Override
+	public TreeMap<String, Integer> getSymptoms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application
+
+Generates a txt file listing and counting each different symptoms seen while taking medication. For analytic purpose.
+
+## Development
+
+Java program built with Eclipse IDE.
+
+## Guideline
+
+Pls run App.java
+
+SymptomListCounter implements interface ISymptomReader and is the proposed correct version of AnalyticsCounter.java
+
+source file : symptoms.txt<br>
+output file : results.out.txt
+
+
+## Usage
+
+Internal usage of Hemebiotech laboratory
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[Hemebiotech laboratory](https://Hemebiotech.com/licenses/Hemebiotech laboratory/)
